3c87f6807b3845596c24259c1abfe930dcd3bbc4
Cedric Kwong <cedrickwong338@yahoo.com>
2025-04-25 19:04:43 -0700

    Initial Commit

---
+++ digital-marketplace/src/test/java/com/handmade_goods/digital_marketplace/DigitalMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handmade_goods.digital_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
