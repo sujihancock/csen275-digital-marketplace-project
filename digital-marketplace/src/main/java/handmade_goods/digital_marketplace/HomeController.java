85961d4dfa81777786f65b5da7103c07b38b1f0c
sujihancock <94586609+sujihancock@users.noreply.github.com>
2025-05-03 15:15:32 -0700

    changed @Controller to @RestController in HomeController.java

--- digital-marketplace/src/main/java/handmade_goods/digital_marketplace/HomeController.java
+++ digital-marketplace/src/main/java/handmade_goods/digital_marketplace/HomeController.java
@@ -2,8 +2,9 @@ package handmade_goods.digital_marketplace;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String home() {
